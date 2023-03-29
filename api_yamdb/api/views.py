c1de81301bdad372484e0a3c94c35818891cd744
Ivan Korbakov <korbakovi@gmail.com>
2023-03-29 20:16:03 +0200

    fix user model

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,19 +11,19 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated,)
 
     def perform_create(self, serializer):
-        if not self.request.user.is_superuser:
+        if self.request.user.role != 1:
             raise PermissionDenied(
                 'Не достаточно прав для создания пользователя!')
         super(UserViewSet, self).perform_create(serializer)
 
     def perform_update(self, serializer):
-        if not self.request.user.is_superuser:
+        if self.request.user.is_superuser != 1:
             raise PermissionDenied(
                 'Не достаточно прав для создания пользователя!')
         super(UserViewSet, self).perform_update(serializer)
 
     def perform_destroy(self, instance):
-        instance = self.get_object()
-        if self.request.user != instance.author:
-            raise PermissionDenied('Удаление чужого контента запрещено!')
+        if self.request.user.is_superuser != 1:
+            raise PermissionDenied(
+                'Не достаточно прав для создания пользователя!')
         return super().perform_destroy(instance)
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,10 +3,20 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    ROLE_CHOICES = (
+        (1, 'admin'),
+        (2, 'user'),
+        (3, 'moderator')
+    )
 
     bio = models.TextField(
         'Биография',
         blank=True,
+    ),
+    role = models.PositiveSmallIntegerField(
+        choices=ROLE_CHOICES,
+        blank=True,
+        null=True
     )
 
     def __str__(self):
