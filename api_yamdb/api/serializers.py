d877104b1aaf6d2c3923968469eead7e54b09190
Константин Бобков <b.konstantv@gmail.com>
2023-04-07 16:45:01 +0300

    after review

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -170,5 +170,13 @@ class TitlesGetSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = ('id', 'name', 'year', 'rating', 'description', 'genre', 'category')
+        fields = (
+            'id',
+            'name',
+            'year',
+            'rating',
+            'description',
+            'genre',
+            'category',
+        )
         model = Title
