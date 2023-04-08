3425d058278dae4629ebacc381f0361155235a9a
deltabobkov <49414123+deltabobkov@users.noreply.github.com>
2023-04-08 09:03:02 +0300

    Update serializers.py

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -25,8 +25,7 @@ class UserSerializer(serializers.ModelSerializer):
         return user
 
     def update(self, instance, validated_data):
-        if validated_data.get('role'):
-            validated_data.pop('role')
+        validated_data.pop('role', None)
         return super().update(instance, validated_data)
 
 
@@ -122,8 +121,8 @@ class TitlesPostSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
         model = Title
+        fields = ('id', 'name', 'year', 'description', 'genre', 'category')
 
 
 class TitlesGetSerializer(serializers.ModelSerializer):
@@ -134,5 +133,13 @@ class TitlesGetSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
         model = Title
+        fields = (
+            'id',
+            'name',
+            'year',
+            'rating',
+            'description',
+            'genre',
+            'category',
+        )
