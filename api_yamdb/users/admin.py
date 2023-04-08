c35e679de436ba22ca98c42a484a3dd8888042c4
Константин Бобков <b.konstantv@gmail.com>
2023-04-08 09:32:16 +0300

    Final fixes 2

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,9 +1,9 @@
-from users.models import User
-
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import Group
+
+from users.models import User
 
 
 class UserCreationForm(forms.ModelForm):
